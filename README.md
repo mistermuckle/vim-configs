620e556c830023332503c64b29a6e29b02fef6fd
Christopher Hoffman <christopherhoffman@WALLL1713.local>
2019-08-26 10:55:05 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# vim-configs
+# VIM Configuration
+
+## Install
+
+```bash
+if [[ -a ~/.vim ]]; then
+  mv ~/.vim ~/.vim.bak
+fi
+if [[ -a ~/.vimrc ]]; then
+  mv ~/.vimrc ~/.vimrc.bak
+fi
+ln -s $PWD/dot-vim ~/.vim
+ln -s $PWD/dot-vimrc ~/.vimrc
+if [[ ! ( -d dot-vim/bundle || -d dot-vim/bundle/Vundle.vim ) ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git dot-vim/bundle/Vundle.vim
+fi
+```
---
+++ dot-vim/skeleton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class XXX extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  componentWillMount() {
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+  }
+
+  render() {
+    return (
+    );
+  }
+}
+
+XXX.propTypes = {
+};
+
+XXX.defaultProps = {
+};
+
+export default XXX;
